df850cf4f427171007c887265ca927a7276a9727
xianpeng.yang2022@gmail.com <xianpeng.yang2022@gmail.com>
2023-09-27 15:46:39 +0800

    add leetcode 反转数字

--- src/main/java/study/alg/leetcode/Solution.java
+++ src/main/java/study/alg/leetcode/Solution.java
@@ -24,7 +24,14 @@ public class Solution {
         }finally {
             return s;
         }
-
+//        public int reverse(int x) {
+//            long n = 0;
+//            while(x != 0) {
+//                n = n*10 + x%10;
+//                x = x/10;
+//            }
+//            return (int)n==n? (int)n:0;
+//        }
 
 
 
